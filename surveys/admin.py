e9989f61b86dcb677e8b85c6747dc854665c853d
sultanmyrza <sultanmyrza@gmail.com>
2016-03-26 17:33:31 +0600

    upgrade admin

--- surveys/admin.py
+++ surveys/admin.py
@@ -2,7 +2,24 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
+from unicefsurvey.settings_local import *
 
-admin.site.register(Survey)
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = QUESTION_PER_SURVEY
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['text', 'survey__title']
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'start_date', 'end_date']
+    inlines = [
+        QuestionInline,
+    ]
+
+
+admin.site.register(Survey, SurveyAdmin)
 admin.site.register(Question)
 admin.site.register(SurveyCity)
--- surveys/models.py
+++ surveys/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from location.models import City
-from django.contrib.auth.models import User
+from staff.models import Worker
 
 ANSWER_CHOICES = (
     ('yes', 'Yes'),
@@ -15,31 +15,40 @@ GENDER_CHOICES = (
 )
 
 # Create your models here.
+
+
 class Survey(models.Model):
-    title = models.CharField(requried=True, max_length=255)
-    start_date = models.DateField(requried=True)
-    end_date = models.DateField(requried=True)
+    title = models.CharField(blank=False, max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __unicode__(self):
+        return self.title
 
 
 class SurveyCity(models.Model):
-    survey = models.ForeignKey(Survey, requried=True)
-    city = models.ForeignKey(City, requried=True)
+    survey = models.ForeignKey(Survey)
+    city = models.ForeignKey(City)
 
 
 class Question(models.Model):
-    survey = models.ForeignKey(Survey, requried=True)
+    survey = models.ForeignKey(Survey)
     text = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.text
 
 
 class Poll(models.Model):
     survey = models.ForeignKey(Survey)
-    user = models.ForeignKey(User)
-    gender = models.CharField(choices=GENDER_CHOICES)
+    user = models.ForeignKey(Worker)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=2)
     age = models.IntegerField()
     lat = models.CharField(max_length=255)
     lng = models.CharField(max_length=255)
 
+
 class Answer(models.Model):
     poll = models.ForeignKey(Poll)
     question = models.ForeignKey(Question)
-    answer = models.CharField(choices=ANSWER_CHOICES)
+    answer = models.CharField(choices=ANSWER_CHOICES, max_length=2)
