f97fc3a46cbeede8f94e8f836e85f00f542f2750
sultanmyrza <sultanmyrza@gmail.com>
2016-03-27 11:44:18 +0600

    django rest

--- unicefsurvey/urls.py
+++ unicefsurvey/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from surveys.views import QuestionViewSet
+from rest_framework import routers
+
+router=routers.DefaultRouter()
+router.register(r'^question', QuestionViewSet)
+
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'unicefsurvey.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^', include(router.urls))
 )
